265ee248dbd1a96928e010f02339e3de5902b34a
kothead <gdmmtf@gmail.com>
2014-06-04 12:39:12 +0600

    created clean project

---
+++ src/com/kothead/no_jb_policy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kothead.no_jb_policy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
